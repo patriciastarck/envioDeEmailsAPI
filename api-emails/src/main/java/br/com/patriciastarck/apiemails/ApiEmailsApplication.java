aa0c24155de7f5cc1645b7826c48c2fc05484fd5
Patricia <patriciastarck@gmail.com>
2024-01-03 17:04:43 -0300

    feat: Commit inicial.

---
+++ api-emails/src/main/java/br/com/patriciastarck/apiemails/ApiEmailsApplication.java
@@ -0,0 +1,13 @@
+package br.com.patriciastarck.apiemails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmailsApplication.class, args);
+	}
+
+}
---
+++ api-emails/src/test/java/br/com/patriciastarck/apiemails/ApiEmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patriciastarck.apiemails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
